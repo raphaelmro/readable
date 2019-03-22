2604bf7a126140c4ad657faa62c253886ce15ce0
Raphael Onofre <raphael.onofre@gmail.com>
2019-03-22 16:50:08 -0300

    Add float and padding left properties into container style

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,15 @@
 import React from "react";
 
+const containerHeaderStyle = {
+  'float': 'left',
+  'padding-left': '4%'
+}
+
 const Header = () => {
   return (
     <section className="hero is-dark">
       <div className="hero-body">
-        <div className="container">
+        <div className="container" style={containerHeaderStyle}>
           <h1 className="title">Twitty Posts</h1>
           <h2 className="subtitle">Anon way to view posts</h2>
         </div>
